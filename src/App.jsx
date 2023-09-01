159b9f14f0907d2a44e9ccc1f116274ceef91291
Priyanka <priyankakri8893@gmail.com>
2023-09-01 20:31:28 +0530

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import "./app.css"
+
 const App= () => {
+    const [count, setCount]= useState(0)
+
     return(
-        <>hello</>
+        <div className="app">
+        <button onClick={ () => setCount(count+1)}>+</button>
+        <span>{count}</span>
+        <button onClick={ () => (count === 0)?setCount(0) : setCount(count-1)}>-</button>
+        </div>
     )
 }
+
 
 export default App;
