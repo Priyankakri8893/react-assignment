184ddabc1c93092fba6af0d13a8606b98b90e3b0
Priyanka <priyankakri8893@gmail.com>
2023-09-02 16:55:40 +0530

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./app.css"
 
+// const App= () => {
+//     const [count, setCount]= useState(0)
+
+//     useEffect( () => {
+//       document.title= count
+//     },[])
+
+//     return(
+//         <div className="app">
+//         <button onClick={ () => setCount(count+1)}>+</button>
+//         <span>{count}</span>
+//         <button onClick={ () => (count === 0)?setCount(0) : setCount(count-1)}>-</button>
+
+//         </div>
+//     )
+// }
+
 const App= () => {
-    const [count, setCount]= useState(0)
-    const [form, setForm]= useState({
-        name: "",
-        email: "",
-        age: ""
-    })
+  const [size, setSize]= useState(window.screen.width)
 
-    const formhandle= (event) => {
-        const name= event.target.name
-        const value= event.target.value
+  const screenResize= () => {
+    setSize(window.innerWidth)
+  }
 
-        setForm( (prev) => {return {...prev, [name]: value}})
+  useEffect( () => {
+    window.addEventListener("resize", screenResize)
+    
+    return () => {
+      window.removeEventListener("resize", screenResize)
     }
-
-    return(
-        <div className="app">
-        <button onClick={ () => setCount(count+1)}>+</button>
-        <span>{count}</span>
-        <button onClick={ () => (count === 0)?setCount(0) : setCount(count-1)}>-</button>
-        {/* ****************************** */}
-
-        <br />
-        <br />
-        <h1>Register</h1>
-        <br />
-        <form action="">
-        name: <input type="text" name="name" value={form.name} onChange={formhandle}/>
-        <br />
-        <br />
-        email: <input type="email" name="email" value={form.email} onChange={formhandle}/>
-        <br />
-        <br />
-        age: <input type="number" name="age" value={form.age} onChange={formhandle}/>
-        <br />
-        <br />
-        <input type="submit" />
-        </form>
-        <h2>my name is {form.name} and age is {form.age}. my email is {form.email}</h2>
-        </div>
-    )
+  }, [size])
+  return(
+    <div className="app">
+      <h1>window size is {size}</h1>
+    </div>
+  )
 }
 
 
